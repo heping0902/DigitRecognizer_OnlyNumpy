8aabfd89d8f02118ed5f707134cafff478234dcb
heping0902 <hepingdeyx@163.com>
2019-08-12 13:32:01 +0000

    Modify the comments in mian py file

--- digitalRecognizer.py
+++ digitalRecognizer.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-# This Python 3 environment comes with many helpful analytics libraries installed
-# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
-# For example, here's several helpful packages to load in 
-
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
-# Input data files are available in the "../input/" directory.
-
+# Input data files are available in the "../input/" directory. And please
+# put your data files in this directory.
 import os
 print(os.listdir("../input"))
 
